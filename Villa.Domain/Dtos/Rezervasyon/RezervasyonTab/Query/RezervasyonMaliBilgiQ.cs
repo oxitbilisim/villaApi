b5899694af81e2846ac2b55b108fdbf324129265
Hakan YILDIRIM <97217890+oxitbilisim@users.noreply.github.com>
2022-07-13 11:41:02 +0300

    cxvx

--- Villa.Domain/Dtos/Rezervasyon/RezervasyonTab/Query/RezervasyonMaliBilgiQ.cs
+++ Villa.Domain/Dtos/Rezervasyon/RezervasyonTab/Query/RezervasyonMaliBilgiQ.cs
@@ -11,12 +11,12 @@ public class RezervasyonMaliBilgiQ
     public Double? ToplamTutar { get; set; }
     public Double? KiralamaKaporasi { get; set; }
     public Double? TahsitTutar { get; set; }
-    public DateTime? TahsilatTarihi { get; set; }
+    public DateTimeOffset? TahsilatTarihi { get; set; }
     public string TahsilatBanka { get; set; }
         
     public Double? KkOdemeTutar { get; set; }
     public Double? EvSahibiOdenenTutar { get; set; }
-    public DateTime? EvSahibineOdemeTarihi { get; set; }
+    public DateTimeOffset? EvSahibineOdemeTarihi { get; set; }
     public Double? Depozito { get; set; }
         
     public Double? KomisyondanIndirim { get; set; }
@@ -30,7 +30,7 @@ public class RezervasyonMaliBilgiQ
     public Double? Konaklama { get; set; }
     public Double? Komisyon { get; set; }
     public string FaturaNumarasi { get; set; }
-    public DateTime FaturaTarihi { get; set; }
+    public DateTimeOffset? FaturaTarihi { get; set; }
     public Double? TransferGeliri { get; set; }
     public Double? RentGeliri { get; set; }
     public Double? YemekSatisi { get; set; }
