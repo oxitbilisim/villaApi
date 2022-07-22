23879b9db49d8d47ed3c11172b636a6ccadb6043
Hakan YILDIRIM <97217890+oxitbilisim@users.noreply.github.com>
2022-07-22 21:18:17 +0300

    kapama

---
+++ Villa/Controllers/KapamaController.cs
@@ -0,0 +1,69 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Villa.Service.Contract;
+using Villa.Domain.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Villa.Domain.Common;
+using Villa.Domain.Dtos;
+using Villa.Domain.Enum;
+using Villa.Service.Implementation;
+
+namespace Villa.Controllers
+{
+    //[Authorize]
+    [ApiController]
+    [Route("api/Kapama")]
+    public class KapamaController : ControllerBase
+    {
+        private readonly RezervasyonService _rezervasyonService;
+
+        public KapamaController(RezervasyonService rezervasyonService)
+        {
+            _rezervasyonService = rezervasyonService;
+        }
+
+        [HttpGet(nameof(GetAll))]
+        public IActionResult GetAll()
+        {
+            var result =  _rezervasyonService.GetAllPI<KapamaDtoQ>(x=> x.IsDeleted == false && x.RezervasyonDurum == RezervasyonDurum.Kapama);
+            if (result is not null)
+            {
+                return Ok(result);
+            }
+            return Ok(result);
+        }
+
+        [HttpGet(nameof(GetById))]
+        public ResponseModel GetById(int id)
+        {
+            var result =  _rezervasyonService.GetPI<KapamaDtoQ>(x=> x.Id == id);
+            if (result is not null)
+            {
+                return new ResponseModel(result);
+            }
+            return new ResponseModel();
+        }
+
+        [HttpPost(nameof(Add))]
+        public  ActionResult<ResponseModel> Add(KapamaDtoC dto)
+        {
+            dto.RezervasyonDurum = RezervasyonDurum.Kapama;
+            return new ResponseModel(_rezervasyonService.Add(dto));
+        }
+        [HttpPut(nameof(Update))]
+        public ActionResult<ResponseModel> Update(KapamaDtoC dto)
+        {
+            dto.RezervasyonDurum = RezervasyonDurum.Kapama;
+            return new ResponseModel( _rezervasyonService.Update(dto));
+        }
+        [HttpDelete(nameof(Delete))]
+        public ActionResult<ResponseModel> Delete(int Id)
+        {
+            return new ResponseModel( _rezervasyonService.Delete(Id));
+        }
+    }
+}
