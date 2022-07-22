d2731721ff89e8e37f143f67efdccc98b6fe2bb7
Hakan YILDIRIM <97217890+oxitbilisim@users.noreply.github.com>
2022-07-22 23:13:13 +0300

    vcx

---
+++ Villa.Domain/Enum/TakvimYarimGun.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Villa.Domain.Enum
+{
+    public enum TakvimYarimGun
+    {
+        [Display(Name = "Başta")]
+        Basta = 1,
+        [Display(Name = "Ortada")]
+        Yok = 2,
+        [Display(Name = "Sonda")]
+        Sonda = 3,
+    }
+}
